5b8209411c5928d11591d7cf615ae8f81dd7f101
Askia Mohamed <askiamk@gmail.com>
2018-09-23 16:18:09 +0200

    Ajout des paramètres de contraintes dans Match et Messages

--- grails-app/domain/fr/mbds/grails/Message.groovy
+++ grails-app/domain/fr/mbds/grails/Message.groovy
@@ -2,6 +2,15 @@ package fr.mbds.grails
 
 class Message {
 
+    User author
+    User target
+    String content
+    Boolean lu = Boolean.FALSE
+
     static constraints = {
+        author nullable: false
+        target nullable: false
+        content nullable: false
     }
+
 }
