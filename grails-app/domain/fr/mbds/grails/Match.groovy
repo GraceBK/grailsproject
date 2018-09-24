6a0a2f3bca3c221532d3045345d0f363b968b17a
Askia Mohamed <askiamk@gmail.com>
2018-09-24 21:08:32 +0200

    Ajout des paramètres de contraintes dans Match

--- grails-app/domain/fr/mbds/grails/Match.groovy
+++ grails-app/domain/fr/mbds/grails/Match.groovy
@@ -6,11 +6,6 @@ class Match {
     int winnerScore
     int looserScore
 
-    User winner
-    User looser
-    int winnerScore
-    int looserScore
-
     Date dateCreated
 
     static constraints = {
